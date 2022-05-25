7d3a97ccdee5e5640dd330fe76677bb19948c6f7
diego <diego.cortinagarcia@gmail.com>
2022-05-25 13:56:19 +0200

    mmm

--- src/main/java/com/capgemini/springBeans/App.java
+++ src/main/java/com/capgemini/springBeans/App.java
@@ -4,11 +4,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.capgemini.beans.Producto;
+
 public class App 
 {
     public static void main( String[] args )
     {
-       ApplicationContext appContext = new ClassPathXmlApplicationContext("com/capgemini/xmls/beans.xml");
+       ApplicationContext appContext = new ClassPathXmlApplicationContext("src/main/resources/beans.xml");
+       
+       Producto chorizo = (Producto) appContext.getBean("producto");
+       System.out.println(chorizo.getNombre());
        
        ((ConfigurableApplicationContext)appContext).close();
     }
