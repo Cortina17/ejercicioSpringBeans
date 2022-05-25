c9d499a71fdfbaf0645f0fc5ab18704a2c56db20
diego <diego.cortinagarcia@gmail.com>
2022-05-25 13:41:53 +0200

    beans

--- src/main/java/com/capgemini/springBeans/App.java
+++ src/main/java/com/capgemini/springBeans/App.java
@@ -1,6 +1,7 @@
 package com.capgemini.springBeans;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
@@ -8,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
        ApplicationContext appContext = new ClassPathXmlApplicationContext("com/capgemini/xmls/beans.xml");
+       
+       ((ConfigurableApplicationContext)appContext).close();
     }
 }
